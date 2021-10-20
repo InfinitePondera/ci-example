64306d290be6b3d940b5f587809d0e7ac967d236
Zyon <zyonamadeus01@gmail.com>
2021-10-19 21:45:52 -0300

    modificado testes para falhar

--- test/utils.test.js
+++ test/utils.test.js
@@ -3,14 +3,14 @@ const { gerarNumeroAleatorio, acharCaracter } = require('../lib/utils');
 describe('Utils', () => {
     describe('gerarNumeroAleatorio', () => {
         test('fim nao pode ser negativo', () => {
-            expect(gerarNumeroAleatorio(20, -5))
+            expect(gerarNumeroAleatorio(20, 25))
                 .toBe(-1);
         });
     });
 
     describe('acharCaracter', () => {
         test('comprimento invalido', () => {
-            expect(acharCaracter(-4, 'abcde', 'c'))
+            expect(acharCaracter(5, 'abcde', 'c'))
                 .toBe('comprimento invalido');
         });
     });    
